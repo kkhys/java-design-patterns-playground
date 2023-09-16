6f6484820b17e0482a13cc00c50db9aecec2f356
ktnkk <ktnkk@pm.me>
2023-09-16 12:20:31 +0900

    refactor(iterator): modify the way the listing is set up

--- iterator/src/main/java/me/kkhys/iterator/App.java
+++ iterator/src/main/java/me/kkhys/iterator/App.java
@@ -1,19 +1,21 @@
 package me.kkhys.iterator;
 
 import me.kkhys.iterator.book.Book;
-import me.kkhys.iterator.book.BookShelf;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        var bookShelf = new BookShelf(5);
-        bookShelf.appendBook(new Book("Around the World in 80 Days"));
-        bookShelf.appendBook(new Book("Bible"));
-        bookShelf.appendBook(new Book("Cinderella"));
-        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
-        bookShelf.appendBook(new Book("East of Eden"));
-        bookShelf.appendBook(new Book("Frankenstein"));
-        bookShelf.appendBook(new Book("Gulliver's Travels"));
-        bookShelf.appendBook(new Book("Hamlet"));
+        var bookShelf = List.of(
+                new Book("Around the World in 80 Days"),
+                new Book("Bible"),
+                new Book("Cinderella"),
+                new Book("Daddy-Long-Legs"),
+                new Book("East of Eden"),
+                new Book("Frankenstein"),
+                new Book("Gulliver's Travels"),
+                new Book("Hamlet")
+        );
 
         var iterator = bookShelf.iterator();
 
